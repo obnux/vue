ba2c9f46bfdb5adfa8790e7629337888245009a9
obnux <obeydi.abb@gmail.com>
2022-09-15 12:26:46 +0200

    Start adding new features

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template lang="">
-  <div class="block" >
+  <div class="block" v-if="showBlock" @click="stopTimer" >
     Click Me
   </div>
 </template>
@@ -11,21 +11,56 @@ export default {
       default: 0
     },
   },
-  data() {
+  data ()
+  {
     return {
-     
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    };
+  },
+  mounted ()
+  {
+
+    setTimeout( () =>
+    {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay );
+  },
+  methods: {
+    startTimer ()
+    {
+      this.timer = setInterval( () =>
+      {
+        this.reactionTime += 10;
+      }, 10 );
+    },
+    stopTimer ()
+    {
+      console.log(this.reactionTime);
+      clearInterval( this.timer );
+      this.showBlock = false;
     }
   },
-}
+  updated ()
+  {
+    console.log( "updated" );
+  },
+  unmounted ()
+  {
+    console.log( "unmounted" );
+  }
+};
 </script>
 <style>
-  .block {
-    width: 400px;
-    border-radius: 20px;
-    background: #0faf87;
-    color: #fff;
-    text-align: center;
-    padding: 100px 0;
-    margin: 40px auto;
-  }
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: #fff;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
 </style>
